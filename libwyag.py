8e37ccde51c9ca0af20b432ab83b65f59b01626c
Pankaj Shrestha <pankajshrestha@gmail.com>
2022-10-16 06:29:02 -0500

    commit ...

--- libwyag.py
+++ libwyag.py
@@ -22,3 +22,47 @@ def main(argv=sys.argv[1:]):
     elif args.command == "commit"           : cmd_commit(args)
     elif args.command == "hash-object"      : cmd_hash_object(args)
     elif args.command == "init"             : cmd_init(args)
+    elif args.command == "ls-tree"          : cmd_ls_tree(args)
+    elif args.command == "merge"            : cmd_merge(args)
+    elif args.command == "rebase"           : cmd_rebase(args)
+    elif args.command == "rev-parse"        : cmd_rev_parse(args)
+    elif args.command == "rm"               : cmd_rm(args)
+    elif args.command == "show-ref"         : cmd_show_ref(args)
+    elif args.command == "tag"              : cmd_tag(args)
+
+
+class GitRepository():
+    """A git respository"""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git repository %s" % path)
+
+        # Read configuration files in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported repositoryformatversion %s" % vers)
+
+
+
+
+def repo_path(repo, *path):
+    """Compute path under repo's gitdir."""
+    return os.path.join(repo.gitdir, *path)

64e009d8444f8e670c972af06ca062d3d7b5a4b3
Pankaj Shrestha <pankajshrestha@gmail.com>
2022-10-30 07:06:30 -0500

    more updates

--- libwyag.py
+++ libwyag.py
@@ -66,3 +66,15 @@ class GitRepository():
 def repo_path(repo, *path):
     """Compute path under repo's gitdir."""
     return os.path.join(repo.gitdir, *path)
+
+
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path, but create dirname(*path) if absent. For example
+    , repo_file(r, \"refs\", \"remotes\", \"origin\",\"HEAD\" will create
+     .git/refs/remotes/origin."""
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as reprot_path, but mkdir *path if absent if mkdir."""
